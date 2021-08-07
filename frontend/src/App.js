b87db4fbfc60ddcb2af50ed3bebcddcaeb51e83f
JeanCSS <jeancordeiro626@gmail.com>
2021-08-07 03:09:36 -0300

    create logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
+import Logon from './pages/logon'
+import React from 'react'; 
+import './global.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello world</h1>
-      </header>
-    </div>
+ 
+  return ( 
+    <Logon>Logon</Logon> 
   );
 }
 
---
+++ frontend/src/pages/logon/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css'
+const Logon = (props) => {
+  return(
+    <h1>{ props.children }</h1>
+  )
+}
+
+export default Logon;
